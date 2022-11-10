bb2e5043d1c4d31a73a2d02b7ede25519734eb90
CamiloSinningUN <camilosinning.cs@gmail.com>
2022-11-09 22:27:31 -0500

    begin bank api

---
+++ README.md
@@ -0,0 +1,25 @@
+# Bank API
+
+This is the Bank API.
+
+## How to run
+
+To run the query API, you need to have installed [Node.js](https://nodejs.org/en/).
+
+Then, you need to install the dependencies:
+
+```bash
+npm install
+```
+
+Remenber to run the database and before running the API.
+
+Finally, you can run the authentication API:
+
+```bash
+npm run dev
+```
+
+## Port
+
+The authentication API will be running on port 3200.
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const axios = require('axios')
+const mysql = require('mysql2')
+const HOST = 'localhost' // Change to actual host
+const cors = require('cors')
+const morgan = require('morgan')
+const PORT = 3200 || process.env.PORT
+const DB_NAME = 'sistemabancos'
+const DB_USER = 'root'  // Change to your DB user
+const DB_PASSWORD = 'root' // Change to your DB password
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
+
+// Create a connection to the mysql database
+const connection = mysql.createConnection({
+    host: HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
+})
+
+// Test database connection
+connection.connect(error => {
+    if (error) throw error
+    console.log('Database connection running!')
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})

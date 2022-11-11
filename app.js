633db7f086df9a7f6e90323dd3f5e03b352a13ba
CamiloSinningUN <camilosinning.cs@gmail.com>
2022-11-11 01:16:44 -0500

    consultas

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 const monitor = require('./monitor.json')
 const fs = require('fs')
-const PORT = 3200 || process.env.PORT
+const PORT = 5000 || process.env.PORT
 const DB_NAME = 'sistemabancos'
 const DB_USER = 'root'  // Change to your DB user
 const DB_PASSWORD = 'root' // Change to your DB password
@@ -30,6 +30,52 @@ connection.connect(error => {
     console.log('Database connection running!')
 })
 
+app.post('/switch', (req, res) => {
+    const body = req.body
+    const { service, enabled } = body
+    monitor[service].enabled = enabled
+    fs.writeFileSync('./monitor.json', JSON.stringify(monitor))
+    res.send('Switched')
+})
+
+// consultar tarjeta
+app.get('/tarjeta-:numero', (req, res) => {
+    // get if consultas is enabled
+    const enabled = monitor.consultas.enabled
+    if (enabled) {
+        const { numero } = req.params
+        const query = `SELECT tarjeta.id, tarjeta.numero, tarjeta.fecha_expiracion, tarjeta.cvv, tarjeta.proveedor, tarjeta.credito, cuenta.nombre_titular FROM tarjeta INNER JOIN cuenta ON tarjeta.id_cuenta = cuenta.id WHERE tarjeta.numero = ${numero}`
+        connection.query(query, (error, result) => {
+            if (error) throw error
+            if (result.length > 0) {
+                res.json(result)
+            } else {
+                res.send('Empty result')
+            }
+        })
+    } else {
+        res.send('Consultas deshabilitadas')
+    }
+})
+
+// consultar cuenta
+app.get('/cuenta-:numero', (req, res) => {
+    const enabled = monitor.consultas.enabled
+    if (enabled) {
+        const { numero } = req.params
+        const query = `SELECT cuenta.id, cuenta.numero, cuenta.nombre_titular, cuenta.email, cuenta.saldo, banco.nombre FROM cuenta INNER JOIN banco ON cuenta.id_banco = banco.id WHERE cuenta.numero = ${numero}`
+        connection.query(query, (error, result) => {
+            if (error) throw error
+            if (result.length > 0) {
+                res.json(result)
+            } else {
+                res.send('Empty result')
+            }
+        })
+    } else {
+        res.send('Consultas deshabilitadas')
+    }
+})
 
 
 app.listen(PORT, () => {

5ce26f0e8f9a4ab16b35e79fce8b22ee143f4618
CamiloSinningUN <camilosinning.cs@gmail.com>
2022-11-23 02:44:52 -0500

    webhook

--- app.js
+++ app.js
@@ -86,58 +86,61 @@ app.post('/transaccion-tarjeta', (req, res) => {
     if (enabled) {
         const body = req.body
         const { numero_tarjeta, numero_cuenta, monto } = body
-        // verificar que la tarjeta exista y tenga saldo
-        const query = `SELECT * FROM tarjeta WHERE numero = ${numero_tarjeta}`
+        let id_origen
+        let id_destino
+
+        const date = new Date()
+        const fecha = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+        const hora = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
+        let query = `SELECT * FROM tarjeta WHERE numero = ${numero_tarjeta}`
         connection.query(query, (error, result) => {
             console.log(query)
             console.log(result)
             if (error) throw error
             if (result.length > 0) {
                 const tarjeta = result[0]
-                if (tarjeta.credito >= monto) {
-                    // verificar que la cuenta exista
-                    const query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta}`
+                id_origen = tarjeta.id
+                query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_tarjeta, completado) VALUES ('${fecha}', '${hora}', '${0}', ${monto}, ${tarjeta.id}, ${false})`
+                connection.query(query, (error, result) => {
+                    if (error) throw error
+                    query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta}`
                     connection.query(query, (error, result) => {
                         if (error) throw error
                         if (result.length > 0) {
                             const cuenta = result[0]
-                            // actualizar tarjeta
-                            let query = `UPDATE tarjeta SET credito = ${tarjeta.credito - monto} WHERE id = ${tarjeta.id}`
+                            id_destino = cuenta.id
+                            query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta, completado) VALUES ('${fecha}', '${hora}', '${1}', ${monto}, ${cuenta.id}, ${false})`
                             connection.query(query, (error, result) => {
                                 if (error) throw error
-                                console.log('Tarjeta actualizada')
-                            }
-                            )
-                            // actualizar cuenta
-                            query = `UPDATE cuenta SET saldo = ${cuenta.saldo + monto} WHERE id = ${cuenta.id}`
-                            connection.query(query
-                                , (error, result) => {
-                                    if (error) throw error
-                                    console.log('Cuenta actualizada')
+                                if (tarjeta.credito >= monto) {
+                                    query = `UPDATE tarjeta SET credito = ${tarjeta.credito - monto} WHERE id = ${tarjeta.id}`
+                                    connection.query(query, (error, result) => {
+                                        if (error) throw error
+                                        query = `UPDATE cuenta SET saldo = ${cuenta.saldo + monto} WHERE id = ${cuenta.id}`
+                                        connection.query(query, (error, result) => {
+                                            if (error) throw error
+                                            // set completado to true
+                                            query = `UPDATE transaccion SET completado = ${true} WHERE id_tarjeta = ${id_origen}`
+                                            connection.query(query, (error, result) => {
+                                                if (error) throw error
+                                                query = `UPDATE transaccion SET completado = ${true} WHERE id_cuenta = ${id_destino}`
+                                                connection.query(query, (error, result) => {
+                                                    if (error) throw error
+                                                    res.send('Transaccion exitosa')
+                                                })
+                                            })
+                                        })
+                                    })
+                                } else {
+                                    res.send('Credito insuficiente')
                                 }
-                            )
-                            const date = new Date()
-                            const fecha = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-                            const hora = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-                            // add transaction of cuenta
-                            query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta) VALUES ('${fecha}', '${hora}', '${1}', ${monto}, ${cuenta.id} )`
-                            connection.query(query, (error, result) => {
-                                if (error) throw error
-                                // add transaction of tarjeta
-                                query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_tarjeta) VALUES ('${fecha}', '${hora}', '${0}', ${monto}, ${tarjeta.id} )`
-
-                                connection.query(query, (error, result) => {
-                                    if (error) throw error
-                                    res.send('Transaccion exitosa')
-                                })
                             })
                         } else {
                             res.send('Cuenta no existe')
                         }
                     })
-                } else {
-                    res.send('Credito insuficiente')
-                }
+                })
             } else {
                 res.send('Tarjeta no existe')
             }
@@ -153,54 +156,60 @@ app.post('/transaccion-cuenta', (req, res) => {
     if (enabled) {
         const body = req.body
         const { numero_cuenta_origen, numero_cuenta_destino, monto } = body
+        let id_origen
+        let id_destino
+
+        const date = new Date()
+        const fecha = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+        const hora = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+
         // verificar que la cuenta origen exista y tenga saldo
-        const query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta_origen}`
+        let query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta_origen}`
         connection.query(query, (error, result) => {
             if (error) throw error
             if (result.length > 0) {
                 const cuenta_origen = result[0]
-                if (cuenta_origen.saldo >= monto) {
-                    // verificar que la cuenta destino exista
-                    const query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta_destino}`
+                id_origen = cuenta_origen.id
+                query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta, completado) VALUES ('${fecha}', '${hora}', '${0}', ${monto}, ${cuenta_origen.id}, ${false})`
+                connection.query(query, (error, result) => {
+                    if (error) throw error
+                    query = `SELECT * FROM cuenta WHERE numero = ${numero_cuenta_destino}`
                     connection.query(query, (error, result) => {
                         if (error) throw error
                         if (result.length > 0) {
                             const cuenta_destino = result[0]
-                            // actualizar cuenta origen
-                            let query = `UPDATE cuenta SET saldo = ${cuenta_origen.saldo - monto} WHERE id = ${cuenta_origen.id}`
+                            id_destino = cuenta_destino.id
+                            query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta, completado) VALUES ('${fecha}', '${hora}', '${1}', ${monto}, ${cuenta_destino.id}, ${false})`
                             connection.query(query, (error, result) => {
                                 if (error) throw error
-                                console.log('Cuenta origen actualizada')
-                            }
-                            )
-                            // actualizar cuenta destino
-                            query = `UPDATE cuenta SET saldo = ${cuenta_destino.saldo + monto} WHERE id = ${cuenta_destino.id}`
-                            connection.query(query, (error, result) => {
-                                if (error) throw error
-                                console.log('Cuenta destino actualizada')
-                            }
-                            )
-                            const date = new Date()
-                            const fecha = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-                            const hora = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-                            // add transaction of cuenta origen
-                            query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta) VALUES ('${fecha}', '${hora}', '${1}', ${monto}, ${cuenta_origen.id} )`
-                            connection.query(query, (error, result) => {
-                                if (error) throw error
-                                // add transaction of cuenta destino
-                                query = `INSERT INTO transaccion (fecha, hora, tipo, monto, id_cuenta) VALUES ('${fecha}', '${hora}', '${0}', ${monto}, ${cuenta_destino.id} )`
-                                connection.query(query, (error, result) => {
-                                    if (error) throw error
-                                    res.send('Transaccion exitosa')
-                                })
+                                if (cuenta_origen.saldo >= monto) {
+                                    query = `UPDATE cuenta SET saldo = ${cuenta_origen.saldo - monto} WHERE id = ${cuenta_origen.id}`
+                                    connection.query(query, (error, result) => {
+                                        if (error) throw error
+                                        query = `UPDATE cuenta SET saldo = ${cuenta_destino.saldo + monto} WHERE id = ${cuenta_destino.id}`
+                                        connection.query(query, (error, result) => {
+                                            if (error) throw error
+                                            // set completado to true
+                                            query = `UPDATE transaccion SET completado = ${true} WHERE id_cuenta = ${id_origen}`
+                                            connection.query(query, (error, result) => {
+                                                if (error) throw error
+                                                query = `UPDATE transaccion SET completado = ${true} WHERE id_cuenta = ${id_destino}`
+                                                connection.query(query, (error, result) => {
+                                                    if (error) throw error
+                                                    res.send('Transaccion exitosa')
+                                                })
+                                            })
+                                        })
+                                    })
+                                } else {
+                                    res.send('Saldo insuficiente')
+                                }
                             })
                         } else {
                             res.send('Cuenta destino no existe')
                         }
                     })
-                } else {
-                    res.send('Saldo insuficiente')
-                }
+                })
             } else {
                 res.send('Cuenta origen no existe')
             }
@@ -209,6 +218,7 @@ app.post('/transaccion-cuenta', (req, res) => {
         res.send('Transacciones deshabilitadas')
     }
 })
+
 
 
 app.listen(PORT, () => {
